14f04642eef5249bac85758251a9ea6d45d760a3
Thaweewat <thaweewat.data@gmail.com>
2023-03-05 00:40:37 +0700

    Add upload csv feature

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
 import streamlit as st
 import pandas as pd
+from io import StringIO
 
 @st.cache
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
 
-df = pd.DataFrame(
-    [
-       {"command": "st.selectbox", "rating": 4, "is_widget": True},
-       {"command": "st.balloons", "rating": 5, "is_widget": False},
-       {"command": "st.time_input", "rating": 3, "is_widget": True},
-   ]
-)
+
+uploaded_file = st.file_uploader("Choose your CSV file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+    edited_df = st.experimental_data_editor(df)
 
 
-edited_df = st.experimental_data_editor(df)
-favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
 st.markdown("You can download edited file from download button below (CSV)")
-
-df_csv = convert_df(edited_df)
-
 st.download_button(
     label="Download edited data as CSV",
-    data=df_csv,
+    data=convert_df(edited_df),
     file_name='edited_data.csv',
     mime='text/csv',
 )
