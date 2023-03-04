ee3ae29e6f7c97762ed9b2267ea2ae48051aa7cf
Thaweewat <thaweewat.data@gmail.com>
2023-03-05 00:30:22 +0700

    Add download button

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
 
 df = pd.DataFrame(
     [
@@ -14,3 +18,12 @@ df = pd.DataFrame(
 edited_df = st.experimental_data_editor(df)
 favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
 st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
+
+df_csv = convert_df(edited_df)
+
+st.download_button(
+    label="Download data as CSV",
+    data=df_csv,
+    file_name='large_df.csv',
+    mime='text/csv',
+)
