444be12708a66d0dacba26253ea4e8da11d6ddec
Thaweewat <thaweewat.data@gmail.com>
2023-03-05 01:35:11 +0700

    Reformat code

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import streamlit as st
 import pandas as pd
-from io import StringIO
 
 # Use the full page instead of a narrow central column
 st.set_page_config(layout="wide")
+
 
 @st.cache
 def convert_df(dataframe: pd.DataFrame):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return dataframe.to_csv().encode('utf-8')
 
+
 # Todo list add features: drop dup, drop na, fill na
 uploaded_file = st.file_uploader("Choose your CSV file")
+
 if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
     df = pd.read_csv(uploaded_file)
@@ -21,8 +23,6 @@ else:
 options = st.multiselect(
     'Select columns to fill NaN',
     list(df))
-
-st.write('Columned selected:', options)
 
 agree = st.checkbox('Fill NaN')
 if agree:
@@ -37,6 +37,4 @@ if final_df is not None:
         data=final_df,
         file_name='edited_data.csv',
         mime='text/csv',
-        )
-
-
+    )
